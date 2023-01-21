3f5dfc3e5104dc7bcfd2524adf9abc534f0a9d54
rahfathoni <rahfathoni@gmail.com>
2023-01-21 22:01:56 +0700

    typo footer; update table

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -3,7 +3,7 @@
     class="footer row wrap justify-center items-start content-start q-py-sm bg-color-red-v1"
   >
     <div class="text-grey-2" style="font-size: 15px">
-      © Copyright 2023. Wiki Football Club All rights reseved
+      © Copyright 2023. Wiki Football Club All rights reserved
     </div>
   </q-footer>
 </template>
